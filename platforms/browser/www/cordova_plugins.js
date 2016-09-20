8a93115657e895ffb0d58bdf6f9414b31835a004
AE <aaronep@admins-MacBook-Air.local>
2016-09-20 18:33:23 -0400

    Clean Up

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -9,14 +9,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/cordova-plugin-tts/www/tts.js",
-        "id": "cordova-plugin-tts.tts",
-        "pluginId": "cordova-plugin-tts",
-        "clobbers": [
-            "TTS"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-ble-central/www/ble.js",
         "id": "cordova-plugin-ble-central.ble",
         "pluginId": "cordova-plugin-ble-central",
@@ -42,7 +34,6 @@ module.exports.metadata =
     "cordova-plugin-statusbar": "1.0.1",
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-custom-config": "2.0.3",
-    "cordova-plugin-tts": "0.2.3",
     "cordova-plugin-ble-central": "1.1.2"
 }
 // BOTTOM OF METADATA
