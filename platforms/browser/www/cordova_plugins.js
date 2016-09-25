cf043c5ccd793b29212c5111198881d10233a8e9
AE <aaronep@live.com>
2016-09-25 06:42:03 -0400

    Browser Platform

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-ble-central/www/ble.js",
         "id": "cordova-plugin-ble-central.ble",
         "pluginId": "cordova-plugin-ble-central",
@@ -25,6 +17,14 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-tts/www/tts.js",
         "id": "cordova-plugin-tts.tts",
         "pluginId": "cordova-plugin-tts",
@@ -36,14 +36,14 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "cordova-custom-config": "2.0.3",
     "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-ble-central": "1.1.2",
     "cordova-plugin-console": "1.0.3",
     "cordova-plugin-geolocation": "2.2.0",
     "cordova-plugin-statusbar": "1.0.1",
-    "cordova-plugin-whitelist": "1.2.2",
-    "cordova-custom-config": "2.0.3",
-    "cordova-plugin-ble-central": "1.1.2",
-    "cordova-plugin-tts": "0.2.3"
+    "cordova-plugin-tts": "0.2.3",
+    "cordova-plugin-whitelist": "1.2.2"
 }
 // BOTTOM OF METADATA
 });
